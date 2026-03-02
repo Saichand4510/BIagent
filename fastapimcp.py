7b6476a4c2b54e1d4214e8735ba926e3c48da03a
SaiChand Linga <saichandlinga@gmail.com>
2026-03-02 19:34:30 +0530

    changed in fastapimcp

--- fastapimcp.py
+++ fastapimcp.py
@@ -9,9 +9,17 @@ from backend import (
     create_chatbot,
     retrieve_all_threads,
 )
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="LangGraph MCP API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 chatbot = None
 
 
